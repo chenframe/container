3e9f2e1e4ba99e7742b6ec143b42c649e3e59068
chenxiansen <15951982354@163.com>
2020-06-15 20:59:18 +0800

    init

---
+++ Container.php
@@ -0,0 +1,27 @@
+<?php
+namespace Chenframe\Container;
+use Chenframe\Promise\Container\Container as ContainerPromise;
+
+class Container implements ContainerPromise
+{
+
+    public function bind($abstract)
+    {
+        // TODO: Implement bind() method.
+    }
+
+    public function make($abstract, array $parameters = [])
+    {
+        // TODO: Implement make() method.
+    }
+
+    public function get($id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function has($id)
+    {
+        // TODO: Implement has() method.
+    }
+}
